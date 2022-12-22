7732d30a326ce9a8c24a661f8dd93ff623ba5985
Wokhollogram <82580173+Wokhollogram@users.noreply.github.com>
2022-12-22 23:27:26 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,115 @@
+'use strict';
+//№1
+
+let opearation;
+
+
+let a1 = 7 + 2
+let a2 = 7 -3
+let a3 = 2 * 4
+let a4 = 4 / 2 
+console.log(`a1 = ${a1}, a2 = ${a2}, a3 = ${a3}, a4 = ${a4}`)
+
+//№2
+
+let height = 23
+let width = 10
+console.log(`s = ${height * width}`)
+
+
+//№3
+
+let b1 = 5 % 3
+let b2 = 3 % 5
+let b3 = 5 + '3'
+let b4 = 75 + 'кг'
+console.log(`b1 = ${b1}, b2 = ${b2}, b3 = ${b3}, b4 = ${b4}`)
+
+//№4
+
+let heightC = 10, dc = 4/2,v;
+v = 3.14 * dc**2*heightC;
+console.log(`v = ${v}`);
+
+
+//№5
+
+let a5 = 10
+let b5 = 2
+console.log(`сумма = ${a5 + b5}, разность = ${a5 - b5}, произведение = ${a5 * b5}, частное = ${a5 / b5}`)
+
+
+//№6
+
+let c = 15
+let d = 2
+let result = c + d
+console.log(`result = ${result}`)
+
+
+//№7
+
+let a7 = 10
+let b7 = 2
+let c7 = 5
+console.log(`сумма переменных = ${a7 + b7 + c7}`)
+
+
+//№8
+
+let a8 = 17
+let b8 = 10
+let c8 = a8 - b8
+let d8 = 7
+let result8 = c8 + d8
+console.log(`значение переменной result = ${result}`)
+
+
+//№9
+
+let name1 = 'Алексей'
+console.log(`Привет, ${name1}!`);
+
+
+//№10
+
+let age = 19;
+console.log(`Мне почти ${age} лет!`);
+
+
+//№11
+
+let str = 'adcde'
+console.log(str.charAt(0));
+console.log(str.charAt(2));
+console.log(str.charAt(4));
+
+
+//№12
+
+let s = 1, h, d3, m;
+h = s * 60 * 60;
+d3 = h * 24;
+m = d3 * 30;
+console.log('Сек. в часе ', h);
+console.log('Сек. в сутках ', d3);
+console.log('Сек. в месяце ', m);
+
+
+//№13
+
+let num = 47;
+num += 7;
+num -= 18;
+num *= 10;
+num /= 15;
+console.log(num);
+
+
+//№14
+
+let num2 = 10;
+num2 ++;
+num2 ++;
+num2 --;
+console.log(num2);
